c290e80965b0f37d0f2d40b54fbc49b8944dfe82
Enrique <antonio.garcia.gallur@gmail.com>
2018-05-01 18:42:37 +0200

    Signed-off-by: Enrique <antonio.garcia.gallur@gmail.com> Test completado

--- EDD-master/FP/src/main/java/fp/Calculator.java
+++ EDD-master/FP/src/main/java/fp/Calculator.java
@@ -6,50 +6,49 @@ import java.util.List;
 
 public class Calculator {
 
-
 	/*
-     * este metodo devuelve el Class del object que le pasamos
-     */
+	 * este metodo devuelve el Class del object que le pasamos
+	 */
 	public static Class classTypeOf(Object x) {
-		throw  new NotImplementedException();
+		throw new NotImplementedException();
 	}
 
-
 	/*
-     * devuelve una lista con los n números de la serie de fibonacci.
-     */
+	 * devuelve una lista con los n números de la serie de fibonacci.
+	 */
 	public static List<Integer> fibonacci(int n) {
-		throw  new NotImplementedException();
+		throw new NotImplementedException();
 	}
 
 	/*
 	 * Escribir todos los números del number al 0 de step en step.
 	 */
 	public static int[] stepThisNumber(int number, int step) {
-		throw  new NotImplementedException();
-		}
+		throw new NotImplementedException();
+	}
 
 	/*
 	 * Módulo al que se le pasa un número entero del 0 al 20 y devuelve los
 	 * divisores que tiene.
 	 */
 	public static int[] divisors(int n) {
-		throw  new NotImplementedException();
+		throw new NotImplementedException();
 	}
 
 	/*
-	 * Toma como parámetros una cadena de caracteres y devuelve cierto si la cadena resulta ser un palíndromo
+	 * Toma como parámetros una cadena de caracteres y devuelve cierto si la cadena
+	 * resulta ser un palíndromo
 	 */
 	public static boolean checkIsPalindrome(String cadena) {
-		throw  new NotImplementedException();
+		throw new NotImplementedException();
 	}
 
 	/*
-	 * Pedir un número de 0 a 99 y mostrarlo escrito. Por ejemplo, para 56
-	 * mostrar: cincuenta y seis
+	 * Pedir un número de 0 a 99 y mostrarlo escrito. Por ejemplo, para 56 mostrar:
+	 * cincuenta y seis
 	 */
 	public static String speakToMe(int n) {
-		throw  new NotImplementedException();
+		throw new NotImplementedException();
 	}
 
 	/*
@@ -57,13 +56,21 @@ public class Calculator {
 	 * dd-MM-yyyy
 	 */
 	public static boolean isLeapYear(String fecha) {
-		throw  new NotImplementedException();
+		if (fecha != "" && fecha != null) {
+			String fechacorta = fecha.substring(6, 10);
+			int fechica = Integer.parseInt(fechacorta);
+			System.out.println(fecha);
+			if (fechica % 4 == 0 && fechica % 100 != 0 || fechica % 400 == 0) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 	/*
 	 * este metodo devuelve cierto si la fecha es válida
 	 */
 	public static boolean isValidDate(String date) {
-		throw  new NotImplementedException();
+		throw new NotImplementedException();
 	}
 }
